76c80f907474c5bea3dd829e09b25b09721abe90
Anatoliy Cherevach <anatoliycherevach@gmail.com>
2020-09-27 15:15:50 +0300

    init server

--- server.go
+++ server.go
@@ -1,1 +1,16 @@
 package main
+import (
+	"log"
+	"net/http"
+	
+)
+​
+func main() {
+​
+
+​
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {}
+	
+	log.Println("Staring HTTP server...")
+	log.Fatal(http.ListenAndServe(":8795", nil))
+}
